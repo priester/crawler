de9ef3e41d7a2826007708d133d9d14fd6e1e9c7
priester <853018334@qq.com>
2018-03-30 00:22:57 +0800

    去除标签

--- crawler/src/main/java/com/priester/crawler/news/process/Test.java
+++ crawler/src/main/java/com/priester/crawler/news/process/Test.java
@@ -2,6 +2,6 @@ package com.priester.crawler.news.process;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("http://www.315online.com/tousu/redian/382689.html".matches("http://www.315online.com/tousu/redian/[0-9]{6}.html"));
+		System.out.println("http://www.smelzh.gov.cn/qyxw/list.php?catid=32&page=3".matches(".*itemid=.*"));
 	}
 }
